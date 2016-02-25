74b85d6fa9215ffa1a19a9e9e34e5d0af8c4dcf2
dkanarek12 <dkanarek12@gmail.com>
2016-02-25 15:10:02 -0500

    Update advanced_python_regex.py
    updated degrees_freq and title_freq functions with the use of regex

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -25,20 +25,20 @@ def find_frequencies(parsed_data): #returns a histogram of the list of data in a
 			d[i] = 1
 	return d
 
+
 def degrees_freq(parsed_data):
 	d = dict() #dictionary of frequencies with the column header as the key
 	for i in parsed_data:
-		#regex = re.compile(('.'))
-		#x=regex.sub('',i[columnIndex])
-		items =i[1].translate(maketrans('',''),'.')
-		items = items.split(' ')
+		items = i[1].split(' ')
 		for j in items:
+			j = re.sub('\.','',j)
+			print j
 			if j in d:
 				d[j] += 1
 			elif j not in d and len(j)>1:
 				d[j] = 1
 	return d
-
+	
 def return_column(parsed_data,columnIndex): #returns a single list of a specified column of the data
 	column = []
 	for i in parsed_data:
@@ -52,11 +52,14 @@ def email_freq(parsed_data):
 		name,dom = x.split('@')
 		domains.append(dom)
 	return find_frequencies(domains)
+	
 
 def title_freq(parsed_data):
 	full_titles = return_column(parsed_data,2)
 	titles =[]
+	pattern = r'\s\w{2}\s'
 	for x in full_titles:
-		index = x.find("Biostatistics")
-		titles.append(x[:index-4])
+		match = re.search(pattern,x)
+		print match.start()
+		titles.append(x[:match.start()])
 	return find_frequencies(titles)
